853ef0d4b6954f33125de4acde5f2865c711ca37
morettipdr <morettipdr@gmail.com>
2022-11-12 11:21:08 -0300

    commiting more changes to mobile and web nlw applications

--- nlw-copa/front-end/src/pages/index.tsx
+++ nlw-copa/front-end/src/pages/index.tsx
@@ -1,40 +1,66 @@
-// interface HomeProps {
-//   count: number;
-// }
-
 import Image from "next/image"
 import appPreviewImg from "../assets/app-preview.png"
 import logoImg from "../assets/logo.svg"
 import userAvatarExampleImg from "../assets/users-avatar-examples.png"
 import iconCheckImage from "../assets/check-icon.svg"
+import { api } from "../lib/axios"
+import { FormEvent, useState } from "react"
 
-export default function Home() {
+interface HomeProps {
+  poolCount: number;
+  guessCount: number;
+  userCountResponse: number;
+}
+
+export default function Home(props: HomeProps) {
+  const [poolTitle, setPoolTitle] = useState("");
+
+async function createPool(event: FormEvent) {
+  event.preventDefault();
+
+  try {
+    const response = await api.post("/pools", {
+      title: poolTitle,
+    })
+    const {code} = response.data;
+    await navigator.clipboard.writeText(code)
+    alert("Bolão criado com sucesso, código copiado para a área de transferência")
+    setPoolTitle("")
+  } catch(e) {
+    console.log(e);
+    alert("falha criando bolão")
+  }
+}
+
   return (
-    <div className="max-w-[1124px] mx-auto grid grid-cols-2 items-center h-screen">
+    <div className="max-w-[1124px] mx-auto grid grid-cols-2 items-center h-screen gap-28">
       <main>
         <Image src={logoImg} alt="logo nlw-copa"></Image>
         <h1 className="mt-14 text-white text-5xl font-bold leading-tight">Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
         <div className="mt-10 flex items-center gap-2">
           <Image src={userAvatarExampleImg} alt=""></Image>
-          <strong className="text-gray-100 text-xl"><span className="text-ignite-500">+12.592 pessoas</span> já estão usando</strong>
+          <strong className="text-gray-100 text-xl"><span className="text-ignite-500">+{props.userCountResponse}</span> já estão usando</strong>
         </div>
-        <form className="mt-10 flex gap-2" action="">
-          <input className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm" type="text" placeholder="Qual nome do seu bolão?" name="" id="" required/>
-          <button className="bg-yellow-500 px-6 py-4 rounded font-bold text-gray-900 text-small uppercase" type="submit">Criar meu bolão</button>
+        <form onSubmit={createPool} className="mt-10 flex gap-2" action="">
+          <input value={poolTitle} onChange={event => {setPoolTitle(event.target.value)}} className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100" type="text" placeholder="Qual nome do seu bolão?" name="" id="" required/>
+          <button className="bg-yellow-500 px-6 py-4 rounded font-bold text-gray-900 text-small uppercase hover:bg-yellow-700" type="submit">Criar meu bolão</button>
         </form>
-        <p>Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
-        <div>
-          <div>
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀</p>
+        <div className="mt-10 pt-10 border-t border-gray-600 flex justify-between text-gray-100">
+          <div className="flex items-center gap-6">
             <Image src={iconCheckImage} alt="iconcheck"></Image>
-            <div>
-              <span>+2.034</span>
-              <span>Bolöes criados</span>
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+{props.poolCount}</span>
+              <span>Bolões criados</span>
             </div>
           </div>
-          <div>
+          <div className="w-px h-14 bg-gray-600"></div>
+          <div className="flex items-center gap-6">
             <Image src={iconCheckImage} alt="iconcheck"></Image>
-            <span>+192.847</span>
-            <span>Palpites enviados</span>
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+{props.guessCount}</span>
+              <span>Palpites enviados</span>
+            </div>
           </div>
         </div>
       </main>
@@ -44,12 +70,15 @@ export default function Home() {
   )
 }
 
-// export const getServerSideProps = async () => {
-//   const response = await fetch("http://localhost:3333/pools/count")
-//   const data = await response.json()
-//   return{
-//     props:{
-//       count: data.count,
-//     }
-//   }
-// }
+export const getServerSideProps = async () => { //APRENDER A USAR getStaticProps NO NEXT.JS PARA NAO FAZER ESSAS CHAMADAS TODO REFRESH.
+  const [poolCountResponse, guessCountResponse, userCountResponse] = await Promise.all([
+    api.get("/pools/count"), api.get("/guesses/count"), api.get("/users/count")
+  ])
+  return{
+    props:{
+      poolCount: poolCountResponse.data.count,
+      guessCount: guessCountResponse.data.count,
+      userCountResponse: userCountResponse.data.count,
+    }
+  }
+}
--- nlw-copa/front-end/tailwind.config.js
+++ nlw-copa/front-end/tailwind.config.js
@@ -8,18 +8,23 @@ module.exports = {
       fontFamily: {
         sans: "Roboto, sans-serif",
       },
+      backgroundImage: {
+        app: "url(/app-bg.png)"
+      },
       colors: {
         gray: {
           900: "#121214",
           100: "#E1E1E6",
           800: "#202024",
-          600: "#323238"
+          600: "#323238",
+          300: "#8D8D99",
         },
         ignite: {
           500: "#129E57"
         },
         yellow: {
-          500: "#F7DD43"
+          500: "#F7DD43",
+          700: "#E5CD3D"
         }
       }
     },
