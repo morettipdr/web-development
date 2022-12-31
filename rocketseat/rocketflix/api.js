d1791dc9067b02762eb3fa32da90608f947726e8
morettipdr <morettipdr@gmail.com>
2022-12-31 12:07:37 -0300

    commiting new completed rocketseat challenge 'rocketflix'

--- rocketseat/rocketflix/api.js
+++ rocketseat/rocketflix/api.js
@@ -3,4 +3,4 @@
 export const API_KEY = 'api_key=cc2742ac993764420fa5fedf5ac980a0';
 export const BASE_URL = 'https://api.themoviedb.org/3/movie/';
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-export const language = 'language=pt-BR';
+export const language = '&language=pt-BR';
--- rocketseat/rocketflix/main.js
+++ rocketseat/rocketflix/main.js
@@ -12,14 +12,21 @@ const movieDetails = document.querySelector(".movieDetails");
 
 button.addEventListener("click", async () => {
   let posterPath;
-  const movieID = Math.floor(Math.random() * 2000)
-  await fetch(BASE_URL + movieID + "?" + API_KEY + "&language=pt-br").then(response => response.json()).then(data => {
-    console.log(data);
-    posterPath = data.poster_path;
-    movieTitle.textContent = data.title
-    movieDetails.textContent = data.overview
-    movie.hidden = false;
-    movieImage.hidden = false;
-  }).catch(e => console.log(e))
-    movieImage.src = IMG_URL + posterPath
+  let noMovie = true
+  while(noMovie){
+    const movieID = Math.floor(Math.random() * 40000)
+    await fetch(BASE_URL + movieID + "?" + API_KEY + language).then(response => response.json()).then(data => {
+      console.log(data);
+      posterPath = data.poster_path;
+      movieTitle.textContent = data.title
+      movieDetails.textContent = data.overview
+      movie.style.display = "flex";
+      noMovie = false;
+      movieImage.src = IMG_URL + posterPath
+      if(data.success == false) {
+        movieImage.src = "./assets/Poster.jpg"
+        movieTitle.innerHTML = "Ops, hoje não é dia de assistir filme. <br> Bora codar! 🚀"
+      }
+    }).catch(e => console.log(e))
+    }
 })
